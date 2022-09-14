45a278b50a3c2299fd75c50bbaf0cf6d991611bf
Areeb Ahmad <paradoxmr24@gmail.com>
2022-09-14 22:29:19 +0530

    initial commit

---
+++ dist/hooks/useSnack.js
@@ -0,0 +1,45 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = useSnack;
+
+require("core-js/modules/web.dom-collections.iterator.js");
+
+var _material = require("@mui/material");
+
+var _react = _interopRequireWildcard(require("react"));
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+const snackReducer = function snackReducer(state, action) {
+  if (!action) return {
+    success: "Closed",
+    show: false
+  };
+  const severity = Object.keys(action)[0];
+  const message = Object.values(action)[0];
+  return {
+    show: true,
+    severity,
+    message
+  };
+};
+
+function useSnack() {
+  const [snack, showMessage] = (0, _react.useReducer)(snackReducer, snackReducer(null));
+  return {
+    snackBar: /*#__PURE__*/_react.default.createElement(_material.Snackbar, {
+      open: snack.show,
+      autoHideDuration: 3000,
+      onClose: () => showMessage(null)
+    }, /*#__PURE__*/_react.default.createElement(_material.Alert, {
+      color: snack.severity,
+      severity: snack.severity
+    }, snack.message)),
+    showMessage
+  };
+}
---
+++ src/lib/hooks/useSnack.js
@@ -0,0 +1,23 @@
+import { Alert, Snackbar } from "@mui/material";
+import React, { useReducer } from "react";
+
+const snackReducer = function (state, action) {
+    if (!action) return { success: "Closed", show: false };
+    const severity = Object.keys(action)[0];
+    const message = Object.values(action)[0];
+    return { show: true, severity, message };
+};
+
+export default function useSnack() {
+    const [snack, showMessage] = useReducer(snackReducer, snackReducer(null));
+    return {
+        snackBar: (
+            <Snackbar open={snack.show} autoHideDuration={3000} onClose={() => showMessage(null)}>
+                <Alert color={snack.severity} severity={snack.severity}>
+                    {snack.message}
+                </Alert>
+            </Snackbar>
+        ),
+        showMessage,
+    };
+}
