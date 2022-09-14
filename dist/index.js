4fc47782ea85f2119b3ebb7b3a8ed61f42508051
Areeb Ahmad <paradoxmr24@gmail.com>
2022-09-14 22:39:18 +0530

    first stable version

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "useSnack", {
+  enumerable: true,
+  get: function get() {
+    return _useSnack.default;
+  }
+});
+
+var _useSnack = _interopRequireDefault(require("./hooks/useSnack"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
---
+++ src/lib/index.js
@@ -0,0 +1,3 @@
+import useSnack from "./hooks/useSnack";
+
+export { useSnack };
